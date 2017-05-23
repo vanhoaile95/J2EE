34e8cda9badd62f452c0f3ed18ca585667c7b62b
DESKTOP-O8A7322\Binh Nguyen <jenova2010.2010@gmail.com>
2017-05-23 15:57:31 +0700

    Update
    Xong phần thống kê sản phẩm

---
+++ WebBanHangJSF/src/java/Model/ThongKeDoanhSo.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Binh Nguyen
+ */
+@ManagedBean(name = "thongke")
+@RequestScoped
+public class ThongKeDoanhSo {
+
+    private String TenSanPham;
+    private int SL;
+    public String thoigian;
+    private String MaSP;
+
+    public String getMaSP() {
+        return MaSP;
+    }
+
+    public void setMaSP(String MaSP) {
+        this.MaSP = MaSP;
+    }
+
+    public String getTenSP() {
+        return TenSanPham;
+    }
+
+    public void setTenSP(String tenSP) {
+        this.TenSanPham = tenSP;
+    }
+
+    public int getSL() {
+        return SL;
+    }
+
+    public void setSL(int SL) {
+        this.SL = SL;
+    }
+
+    public String getThoigian() {
+        return thoigian;
+    }
+
+    public void setThoigian(String thoigian) {
+        this.thoigian = thoigian;
+    }
+
+}
